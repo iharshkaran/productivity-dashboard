27180c9e11c265da4614348d4ea5a58e0620de61
iharshkaran <harshkaranofficial@gmail.com>
2026-06-11 23:32:13 +0530

    Productivity Dashboard

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function openFeatures() {
+    var cards = document.querySelectorAll(".card");
+    var cardPage = document.querySelectorAll(".cardPage");
+    var cardPageBackBtn = document.querySelectorAll(".close-btn")
+
+    cards.forEach(function(card){
+        card.addEventListener("click",function(){
+            cardPage[card.id].style.display="block";
+        })
+    })
+
+    cardPageBackBtn.forEach(function(backBtn){
+        backBtn.addEventListener("click",function(){
+            cardPage[backBtn.id].style.display="none";
+        })
+    })
+}
+
+openFeatures()
