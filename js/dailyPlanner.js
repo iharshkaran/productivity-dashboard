6629e2f90a8c5a54681bff4a13e91ab03ff251bf
iharshkaran <harshkaranofficial@gmail.com>
2026-06-18 08:43:54 +0530

    Productivity Dashboard

--- js/dailyPlanner.js
+++ js/dailyPlanner.js
@@ -5,20 +5,38 @@ function dailyPlanner(){
     for(let i=0; i<18; i++){
         hours.push(`${6+i}:00 - ${7+i}:00`)
     }
-
+    
+    let dailyPlanner = document.querySelector(".daily-planner");
+    let dailyPlannerInputData =JSON.parse(localStorage.getItem("dailyPlannerInputData")) || []
+    
     let wholeDaySum = ''
-    hours.forEach((elem)=>{
+    
+    hours.forEach((elem,idx)=>{
+
+        let savedData = dailyPlannerInputData[idx] || ''
+        
         wholeDaySum += `<div class="daily-planner-time">
-                            <div class="planner-time">
-                                <p>${elem}</p>
-                            </div>
-                            <div class="planner-task"><input type="text" placeholder="..."></div>
-                        </div>`
+        <div class="planner-time">
+        <p>${elem}</p>
+        </div>
+        <div class="planner-task"><input id="${idx}" type="text" placeholder="..." value="${savedData}"></div>
+        </div>`
 
     })
-
-    let dailyPlanner = document.querySelector(".daily-planner");
+    
     dailyPlanner.innerHTML = wholeDaySum
+
+    let dailyPlannerInput = document.querySelectorAll(".daily-planner input");
+    console.log(dailyPlannerInput)
+    dailyPlannerInput.forEach((inputPlanner,idx)=>{
+        inputPlanner.addEventListener("input",()=>{
+            dailyPlannerInputData[idx] = inputPlanner.value;
+            localStorage.setItem("dailyPlannerInputData", JSON.stringify(dailyPlannerInputData))
+        })    
+    })
+
+    
+    
     
 }
 export default dailyPlanner;
